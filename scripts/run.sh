0728f7a174687db765dbbba3229ec2477948eab4
Jaewoopudding <ijnn258@gmail.com>
2025-02-18 22:24:33 +0900

    run script

---
+++ scripts/run.sh
@@ -0,0 +1,33 @@
+CUDA_VISIBLE_DEVICES=0 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 5 --nfe_per_action 16 --num_images 36 --expansion_coef 0.2 --variant PM &
+CUDA_VISIBLE_DEVICES=1 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 8 --nfe_per_action 10 --num_images 36 --expansion_coef 0.2 --variant PM &
+CUDA_VISIBLE_DEVICES=2 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 10 --nfe_per_action 8 --num_images 36 --expansion_coef 0.2 --variant PM &
+CUDA_VISIBLE_DEVICES=3 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 20 --nfe_per_action 4 --num_images 36 --expansion_coef 0.2 --variant PM &
+CUDA_VISIBLE_DEVICES=4 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 40 --nfe_per_action 2 --num_images 36 --expansion_coef 0.2 --variant PM &
+CUDA_VISIBLE_DEVICES=5 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 80 --nfe_per_action 1 --num_images 36 --expansion_coef 0.0 --variant PM &
+wait
+
+CUDA_VISIBLE_DEVICES=0 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 5 --nfe_per_action 16 --num_images 36 --expansion_coef 0.1 --variant PM &
+CUDA_VISIBLE_DEVICES=1 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 8 --nfe_per_action 10 --num_images 36 --expansion_coef 0.1 --variant PM &
+CUDA_VISIBLE_DEVICES=2 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 10 --nfe_per_action 8 --num_images 36 --expansion_coef 0.1 --variant PM &
+CUDA_VISIBLE_DEVICES=3 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 20 --nfe_per_action 4 --num_images 36 --expansion_coef 0.1 --variant PM &
+CUDA_VISIBLE_DEVICES=4 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 40 --nfe_per_action 2 --num_images 36 --expansion_coef 0.1 --variant PM &
+CUDA_VISIBLE_DEVICES=5 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 60 --nfe_per_action 1 --num_images 36 --expansion_coef 0.0 --variant PM &
+wait
+
+CUDA_VISIBLE_DEVICES=0 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 5 --nfe_per_action 16 --num_images 36 --expansion_coef 0.01 --variant PM &
+CUDA_VISIBLE_DEVICES=1 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 8 --nfe_per_action 10 --num_images 36 --expansion_coef 0.01 --variant PM &
+CUDA_VISIBLE_DEVICES=2 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 10 --nfe_per_action 8 --num_images 36 --expansion_coef 0.01 --variant PM &
+CUDA_VISIBLE_DEVICES=3 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 20 --nfe_per_action 4 --num_images 36 --expansion_coef 0.01 --variant PM &
+CUDA_VISIBLE_DEVICES=4 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 40 --nfe_per_action 2 --num_images 36 --expansion_coef 0.01 --variant PM &
+CUDA_VISIBLE_DEVICES=5 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 20 --nfe_per_action 1 --num_images 36 --expansion_coef 0.0 --variant PM &
+wait
+
+CUDA_VISIBLE_DEVICES=0 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 5 --nfe_per_action 16 --num_images 36 --expansion_coef 0.3 --variant PM &
+CUDA_VISIBLE_DEVICES=1 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 8 --nfe_per_action 10 --num_images 36 --expansion_coef 0.3 --variant PM &
+CUDA_VISIBLE_DEVICES=2 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 10 --nfe_per_action 8 --num_images 36 --expansion_coef 0.3 --variant PM &
+CUDA_VISIBLE_DEVICES=3 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 20 --nfe_per_action 4 --num_images 36 --expansion_coef 0.3 --variant PM &
+CUDA_VISIBLE_DEVICES=4 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 40 --nfe_per_action 2 --num_images 36 --expansion_coef 0.3 --variant PM &
+CUDA_VISIBLE_DEVICES=5 python inference_decoding_mcts.py --reward 'aesthetic' --bs 4 --duplicate_size 40 --nfe_per_action 1 --num_images 36 --expansion_coef 0.0 --variant PM &
+wait
+
+
